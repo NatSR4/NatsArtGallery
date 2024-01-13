da79bf2b9a151b04a5d906f2770c770cabb5a7d9
NatSR4 <sneha.ranjit4@gmail.com>
2024-01-13 18:10:13 -0500

    Colorwheel component
    Bare bones website, added color wheel for starters

---
+++ natsartgallery/components/NavBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Wheel from '@uiw/react-color-wheel';
+import { hsvaToHex } from '@uiw/color-convert';
+import Navbar from 'react-bootstrap/Navbar';
+
+export default function NavBar() {
+   
+    <Navbar fixed='top'>
+        Hello! Welcome to Nat's Art Gallery!
+    </Navbar>
+}
